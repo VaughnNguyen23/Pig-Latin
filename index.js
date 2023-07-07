438597d56d7e947e70b92f292817db41e5c75c67
Vaughn Nguyen <vaughnnguyen522@outlook.com>
2023-07-06 18:32:22 -0700

    Added button to translate to pig latin

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 function translatePigLatin(str) {
-    let vowels = "aeiou".split("");
-  
-    for (let i = 0; i < str.length; i += 1) {
-      let individualLetter = str[i];
-      if (vowels.indexOf(individualLetter) != -1 && i === 0) {
-        return str + "way";
-      }
-      if (vowels.indexOf(individualLetter) != -1) {
-        return str.slice(i) + str.slice(0, i) + "ay";
-      }
+  let vowels = "aeiou".split(""); // Array of vowels
+
+  for (let i = 0; i < str.length; i += 1) {
+    let individualLetter = str[i]; // Get the current letter
+    if (vowels.indexOf(individualLetter) != -1 && i === 0) {
+      // If the letter is a vowel and the first letter of the word
+      return str + "way"; // Append "way" and return the translation
     }
-    return str + "ay";
+    if (vowels.indexOf(individualLetter) != -1) {
+      // If the letter is a vowel (not the first letter)
+      return str.slice(i) + str.slice(0, i) + "ay"; // Move consonants to the end and append "ay"
+    }
   }
-  
-  let result = translatePigLatin("apple");
-  console.log(result);
+  return str + "ay"; // If no vowels found, simply append "ay" to the end
+}
+
+let result = translatePigLatin("apple"); // Translate the word "apple"
+console.log(result); // Print the resulting Pig Latin translation
+
